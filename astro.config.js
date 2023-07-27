c978f7dd66952d4ce3107f419a38cd211c696901
Divya <divya.ts@citrusinformatics.com>
2023-07-27 14:36:04 +0530

    update: package

--- astro.config.js
+++ astro.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 // Eventually, replace this import with the official one - we're using a
 // patched version for now.
 // import deno from "@astrojs/deno";
@@ -6,6 +9,7 @@ import deno from "deno-astro-adapter";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: deno(),
 });
