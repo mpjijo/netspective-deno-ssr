95e541a08a042b128affb88535a96fef13901b9c
Jijo M P <jijo.ponnappan@citrusinformatics.com>
2023-08-02 13:41:26 +0530

    fix: update Deno dependencies

--- src/content/technology-services/_collection.ts
+++ src/content/technology-services/_collection.ts
@@ -1,10 +1,6 @@
 import * as ac from 'astro:content';
 import * as s from '../../governance/information-model/schemas';
-import autoRoutes from "./_routes.auto.json";
-export type AutoRouteKey = keyof typeof autoRoutes;
-
 
 export const technologySevicesCollection = ac.defineCollection({ schema: s.consultingSevicesSchema });
-
 
 export default technologySevicesCollection;
